da4e6c362769c32addd236c4c870e35136310778
rogelio1502 <rogelio15021515@gmail.com>
2023-04-20 18:27:51 -0600

    added component package

---
+++ update-ef-package.sh
@@ -0,0 +1,4 @@
+composer update rogelio1502/ef-orchid-package &&
+cp -r vendor/rogelio1502/ef-orchid-package/public/ef public &&
+cp -r vendor/rogelio1502/ef-orchid-package/resources/views/vendor resources/views &&
+cp -r vendor/rogelio1502/ef-orchid-package/resources/views/brand resources/views
