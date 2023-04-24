e1c44e893069785ebc6fd6f44db273344814f51d
rogelio1502 <rogelio15021515@gmail.com>
2023-04-24 11:15:37 -0600

    updated ef package

--- update-ef-package.sh
+++ update-ef-package.sh
@@ -1,4 +1,2 @@
 composer update rogelio1502/ef-orchid-package &&
-cp -r vendor/rogelio1502/ef-orchid-package/public/ef public &&
-cp -r vendor/rogelio1502/ef-orchid-package/resources/views/vendor resources/views &&
-cp -r vendor/rogelio1502/ef-orchid-package/resources/views/brand resources/views
+php artisan vendor:publish --provider="EF\Providers\EfServiceProvider"
